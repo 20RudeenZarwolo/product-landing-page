757bbeacfedd3de818908b9aa6ee1a19212dc1cd
Rudeen N. Zarwolo <rudeenzarwolo@gmail.com>
2024-06-17 18:34:55 +0000

    Added product landing page files

---
+++ product-landing-page.js
@@ -0,0 +1,16 @@
+
+var phoneOne = document.getElementById("phone-one"); 
+var phoneTwo = document.getElementById("phone-two"); 
+var phoneThree = document.getElementById("phone-three");
+
+phoneOne.addEventListener("click", function button(){
+    alert("iPhone 8: This phone cost $700, to buy contact us below.");
+});
+
+phoneTwo.addEventListener("click", function button(){
+    alert("iPhone 8+: This phone cost $100, to buy contact us below.");
+});
+
+phoneThree.addEventListener("click", function button(){
+    alert("iPhone 14: This phone cost $1500, to buy contact us below.");
+});
